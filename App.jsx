1bafc7161979ff79b4653f04819f467f6ee6508c
Komala SR <126228920+komalasr@users.noreply.github.com>
2023-03-04 15:59:05 +0530

    Nav bars
    added

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import Nav from './component/Nav';
+import "./global.css"
+const App =() =>{
+return(
+    <>
+    <Nav/>
+    </>
+)
+}
+export default App
---
+++ Nav.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Logo from './Logo';
+import Menu from './Menu';
+const Nav =() =>{
+return(
+    <>
+    <div className="divblock">
+    <Logo/>
+    <Menu/>
+    </div>
+    </>
+)
+}
+export default Nav
