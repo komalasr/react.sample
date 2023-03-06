c89a78377f4cd071758801fc9003886c92a4ddb2
Komala SR <126228920+komalasr@users.noreply.github.com>
2023-03-06 11:16:29 +0530

    added

---
+++ src/component/Sample1.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+
+import STYLE from "./s1.module.css"
+
+const Sample1 =() =>{
+return(
+    <>
+    <div >
+        <a href="">logo</a>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">About Us</a></li>
+            <li><a href="">Services</a></li>
+            <li><a href="">Blog</a></li>
+            <li><a href="">Contact</a></li>
+        </ul>
+    </div>
+    </>
+)
+}
+export default Sample1
