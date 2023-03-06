57322521a4af488bb50b5c3044fa10a56ed5c018
Komala SR <126228920+komalasr@users.noreply.github.com>
2023-03-06 11:50:25 +0530

    Examples

---
+++ Navbar/src/component/Nav.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+
+import Sample1 from "./Sample1";
+import Sample2 from './Sample2';
+import Sample3 from './Sample3';
+import Sample4 from './Sample4';
+import Sample5 from './Sample5';
+
+
+const Nav =() =>{
+return(
+    <>
+   
+    <Sample1/>
+    <Sample2/>
+    <Sample3/>
+    <Sample4/>
+    <Sample5/>
+    </>
+)
+}
+export default Nav
